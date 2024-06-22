96a5fbb792dae86083e5dfee70d813bb2c24b6ad
ylose <heungeob1003@gmail.com>
2024-06-22 11:40:08 +0900

    인사말을 출력하는 Hello 함수 추가(Row,Column,TextField,Button)

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -2,10 +2,11 @@
 
 package com.example.hellocompose
 
-import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,34 +15,30 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import com.example.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Welcome()
-            Greeting()
+            Hello()
         }
     }
 }
 
 @Composable
-@Preview
 fun Welcome() {
     Text(
         text = stringResource(id = R.string.welcome),
@@ -50,7 +47,6 @@ fun Welcome() {
 }
 
 @Composable
-@Preview
 fun Greeting(name: String = "원치현") {
     Text(
         text = stringResource(id = R.string.hello, name),
@@ -89,6 +85,28 @@ fun TextAndButton(name: MutableState<String>, nameEntered: MutableState<Boolean>
                 nameEntered.value = true
             }) {
             Text(text = stringResource(id = R.string.done))
+        }
+    }
+}
+
+@Composable
+@Preview()
+fun Hello() {
+    val name = remember { mutableStateOf("") } // 상태 생성: mutableStateOf 상태 기억: remember
+    val nameEntered = remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        if (nameEntered.value) {
+            Greeting(name.value)
+        } else {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Welcome()
+                TextAndButton(name, nameEntered)
+            }
         }
     }
 }
