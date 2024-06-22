a49834233eeec9345a1f099b6396a5482eeba77b
ylose <heungeob1003@gmail.com>
2024-06-22 10:58:47 +0900

    Add Text Composable Function

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -1,46 +1,58 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.hellocompose
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import com.example.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Welcome()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+@Preview
+fun Welcome() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = stringResource(id = R.string.welcome),
+        style = MaterialTheme.typography.titleSmall
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HelloComposeTheme {
-        Greeting("Android")
-    }
+@Preview
+fun Greeting(name: String = "원치현") {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleMedium
+    )
 }
